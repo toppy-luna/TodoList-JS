927cefc21a5834d847eef549ff91160eaee5769b
toppy-luna <toppy.luna@gmail.com>
2019-10-23 21:10:18 +0900

    add todoListApp test

--- test/api.ts
+++ test/api.ts
@@ -2,7 +2,7 @@ import Api from '../sandbox/Api';
 
 const assert = require("assert");
 
-describe("Apiのテスト", () => {
+describe.skip("Apiのテスト", () => {
     it("addのテスト", () => {
         const api = new Api();
         assert(5 === api.add(2, 3));
