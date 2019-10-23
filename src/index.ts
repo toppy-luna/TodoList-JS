7fdad89d9e15bfd72fed2397e474a0c2f9b68be2
toppy-luna <toppy.luna@gmail.com>
2019-10-23 17:11:08 +0900

    Replace all function to api

--- src/index.ts
+++ src/index.ts
@@ -9,53 +9,21 @@ app.use(express.static("web"));
 
 const api = new todoListApp.TodoListApp();
 
-const todoList: any[] = [];
-
 app.get("/api/v1/list/", (req, res) => {
     res.json(api.getListAll());
 });
 
 app.post("/api/v1/add", (req, res) => {
-    const todoData = req.body;
-    const todoTitle = todoData.title;
-
-    const id = uuidv4();
-
-    const todoItem = {
-        id,
-        title: todoTitle,
-        done: false
-    };
-
-    todoList.push(todoItem);
-
-    console.log("Add: " + JSON.stringify(todoItem));
-
-    res.json(todoItem);
+    res.json(api.addItem(req.body, uuidv4()));
 });
 
 app.delete("/api/v1/item/:id", (req, res) => {
-    const index = todoList.findIndex((item) => item.id === req.params.id);
-
-    if (index >= 0) {
-        const deleted = todoList.splice(index, 1); // indexの位置にある項目を削除
-        console.log("Delete: " + JSON.stringify(deleted[0]));
-    }
-
+    api.deleteItem(req.params.id);
     res.sendStatus(200);
 });
 
 app.put("/api/v1/item/:id", (req, res) => {
-    const index = todoList.findIndex((item) => item.id === req.params.id);
-
-    if (index >= 0) {
-        const item = todoList[index];
-        if (req.body.done) {
-            item.done = req.body.done === "true";
-        }
-        console.log("Edit: " + JSON.stringify(item));
-    }
-
+    api.setItemDone(req.params.id, req.body.done);
     res.sendStatus(200);
 });
 
