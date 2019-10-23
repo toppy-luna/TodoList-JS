7d815afb11c73799584f2a6c2c1880d0170a8c02
toppy-luna <toppy.luna@gmail.com>
2019-10-23 20:10:13 +0900

    add fail test sample

--- test/api.ts
+++ test/api.ts
@@ -7,4 +7,8 @@ describe("Apiのテスト", () => {
         const api = new Api();
         assert(5 === api.add(2, 3));
     })
+    it("addの失敗テスト", () => {
+        const api = new Api();
+        assert(10 === api.add(2, 3));
+    })
 })
