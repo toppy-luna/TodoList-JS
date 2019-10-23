1b5757c80730f250fce3c9ccc57e2fd6aed84a3e
toppy-luna <toppy.luna@gmail.com>
2019-10-23 16:05:01 +0900

    Replace single quote to double quote, because unify.

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,15 @@ import uuidv4 from "uuid/v4";
 
 const app = express();
 app.use(multer().none());
-app.use(express.static('web'));
+app.use(express.static("web"));
 
 const todoList: any[] = [];
 
-app.get('/api/v1/list', (req, res) => {
+app.get("/api/v1/list", (req, res) => {
     res.json(todoList);
 });
 
-app.post('/api/v1/add', (req, res) => {
+app.post("/api/v1/add", (req, res) => {
     const todoData = req.body;
     const todoTitle = todoData.title;
 
@@ -26,34 +26,34 @@ app.post('/api/v1/add', (req, res) => {
 
     todoList.push(todoItem);
 
-    console.log('Add: ' + JSON.stringify(todoItem));
+    console.log("Add: " + JSON.stringify(todoItem));
 
     res.json(todoItem);
 });
 
-app.delete('/api/v1/item/:id', (req, res) => {
+app.delete("/api/v1/item/:id", (req, res) => {
     const index = todoList.findIndex((item) => item.id === req.params.id);
 
     if (index >= 0) {
         const deleted = todoList.splice(index, 1); // indexの位置にある項目を削除
-        console.log('Delete: ' + JSON.stringify(deleted[0]));
+        console.log("Delete: " + JSON.stringify(deleted[0]));
     }
 
     res.sendStatus(200);
 });
 
-app.put('/api/v1/item/:id', (req, res) => {
+app.put("/api/v1/item/:id", (req, res) => {
     const index = todoList.findIndex((item) => item.id === req.params.id);
 
     if (index >= 0) {
         const item = todoList[index];
         if (req.body.done) {
-            item.done = req.body.done === 'true';
+            item.done = req.body.done === "true";
         }
-        console.log('Edit: ' + JSON.stringify(item));
+        console.log("Edit: " + JSON.stringify(item));
     }
 
     res.sendStatus(200);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(3000, () => console.log("Listening on port 3000"));
